542c7cf3174bccf2cba9e7b3584e244d9c083a25
CoolRy <that007guy@hotmail.com>
2022-07-25 15:06:21 -0400

    fix enum values

--- models/set.js
+++ models/set.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const setSchema = new mongoose.Schema({
   owner: {type: mongoose.Schema.Types.ObjectId, ref:'profile'},
   title: String,
-  category: {type:String, enum: [js, html, css, python]},
+  category: {type:String, enum: ['JS', 'HTML', 'CSS', 'Python']},
   cards: [{type:mongoose.Schema.Types.ObjectId, ref:'card'}]
 },{
   timestamps: true,
